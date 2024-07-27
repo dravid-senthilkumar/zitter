a277a0e0aef93c4e058e1c7f595fc79491938955
dravid-senthilkumar <dravidsenthilkumar16@gmail.com>
2024-07-27 13:44:00 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -8,7 +8,7 @@ const Post = ({post}) => {
       <h2 >{post.title}</h2>
       <p className="postDate">{post.datetime}</p>
       </Link>
-      <p className="postBody">{(post.body).length <= 60 ? post.body : `${(post.body).slice(0,60)}...`}</p>
+      <p className="postBody">{(post.body).length <= 90 ? post.body : `${(post.body).slice(0,90)}...`}</p>
     </article>
   )
 }
